00274db228b76c6ab2afdec3fd0feb02b5ca3b42
William Stach <ashnwill49@gmail.com>
2020-10-08 07:23:51 -0600

    rename to limnemu

--- vm/conf.lua
+++ vm/conf.lua
@@ -2,7 +2,7 @@ function love.conf(t)
 	t.window.width = 640
 	t.window.height = 480
 	t.window.resizable = true
-	t.window.title = "limnvm"
+	t.window.title = "limnemu"
 	jit.opt.start("maxtrace=100000",
 		"maxrecord=40000",
 		"maxside=1000",
